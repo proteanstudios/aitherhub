ad82d0171ab01f8402e8789defd919c9db61dbe8
Duy <nguyenduy05011@gmail.com>
2025-12-29 09:57:52 +0000

    change .env backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,21 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 from app.routers.auth import router as auth_router
 from app.routers.login import router as login_router
 from app.routers.videos import router as videos_router
 
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
