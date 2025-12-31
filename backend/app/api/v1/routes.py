33e6674825800dc0a4de0150cac7b806ba9857b9
tien <tien.tq@proteanstudios.com>
2025-12-31 11:00:17 +0700

    fix route

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -5,4 +5,4 @@ from app.api.v1.endpoints.video import router as video_router
 
 routers = APIRouter()
 routers.include_router(auth_router, prefix="/auth", tags=["Auth"])
-routers.include_router(video_router, prefix="/videos")
+routers.include_router(video_router) 
